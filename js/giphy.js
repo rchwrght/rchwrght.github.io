8998b7adef5772e2ebe0bf35a02a8b02e9f1dc04
Richard Wright <richardwright@Richards-MacBook-Pro.local>
2018-01-20 20:46:59 +0100

    creating a "load more" in GIPHY clone

--- js/giphy.js
+++ js/giphy.js
@@ -1,12 +1,18 @@
 var search = location.search.slice(8);
 
-var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=53WYlS1SZqJ1NOEAgZeQlD6sddrUA013&limit=25", function(data){
+var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + search +"&api_key=53WYlS1SZqJ1NOEAgZeQlD6sddrUA013&limit=25", function(data){
 
 	var images = document.querySelectorAll("img");
 
 	for(var i = 0; i < images.length; i++){
 		images[i].src=data["data"][i]["images"]["fixed_height"]["url"];
 	}
+});
+
+$(".myButton").click(function(){
+	$(".more").slideDown(1000, function(){
+		$(".more").css("opacity", "1");
+	});
 });
 
 $("form").submit(function(){
