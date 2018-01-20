3e0049be6bfd2bb8b88282b7c227e6f25b225180
Richard Wright <richardwright@Richards-MacBook-Pro.local>
2018-01-20 13:52:43 +0100

    small updates

--- js/about.js
+++ js/about.js
@@ -1,5 +1,11 @@
 $(window).scroll(function(){
-	var s = -$(document).scrollTop()/5 - 200;
+	var s = -$(document).scrollTop()/5 + 20;
 	console.log(s);
 	$(".border").css("transform", "translateY(" + s + "px)");
-}) 
+});
+
+$(".underlay").hover(function(){
+	$(".overlay").fadeTo(500, 1.0);
+}, function(){
+	$(".overlay").fadeTo(500, 0);
+})
