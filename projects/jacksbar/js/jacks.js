deabb01a070938c77ef7af14834e9021790064ab
Richard Wright <richardwright@Richards-MacBook-Pro.local>
2018-06-02 23:06:29 +0200

    added Jacks Bar website

---
+++ projects/jacksbar/js/jacks.js
@@ -0,0 +1,26 @@
+var aboutSection = document.querySelector(".about p");
+var musicSection = document.querySelector(".music hr")
+var aboutLis = document.querySelectorAll(".about .grid li");
+var musicLis = document.querySelectorAll(".music .grid li")
+
+window.addEventListener('scroll', function(){
+	isInViewPort(aboutSection, aboutLis);
+	isInViewPort(musicSection, musicLis)
+});
+
+function isInViewPort(elem, item){
+	var bounding = elem.getBoundingClientRect();
+	if (
+		bounding.top >= 0 &&
+		bounding.left >= 0 &&
+		bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+		bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+	) {
+		console.log('In the viewport!');
+		for(var i = 0; i < item.length; i++){
+			item[i].classList.add("animate");
+		}
+	} else {
+		console.log('Not in the viewport... whomp whomp');
+	}	
+};
