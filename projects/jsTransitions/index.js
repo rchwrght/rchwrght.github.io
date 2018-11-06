4bc68a005e74a633ff9a8bced1574dda16e169cf
Rich Wright <rchwrght@gmail.com>
2018-11-06 11:07:23 +0100

    mobile compatibility for scroll transition

--- projects/jsTransitions/index.js
+++ projects/jsTransitions/index.js
@@ -15,3 +15,16 @@ window.addEventListener('mousewheel', function(e){
 			sectionTwo.style.opacity = fade;
 		}
 });
+
+window.addEventListener('touchmove', function(e){
+		if(e.deltaY > 0){
+			fade = fade < 1 ? fade += 0.05 : 1;
+			sectionOne.style.opacity = 1 - fade;
+			sectionTwo.style.opacity = fade;
+			console.log(fade);
+		} else {
+			fade = fade > 0 ? fade -= 0.05 : 0;
+			sectionOne.style.opacity = 1 - fade;
+			sectionTwo.style.opacity = fade;
+		}
+});
