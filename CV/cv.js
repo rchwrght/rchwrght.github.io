9c14b7833aa6c30b5c42db76ef69ba624ba35b59
Rich Wright <rchwrght@gmail.com>
2022-08-02 14:08:58 +0200

    bootleg add
    added bootleg to CV page, inserting accordion feature across work experience section
    still TODO
    - styling of links on accordion
    - updating text of CV

--- CV/cv.js
+++ CV/cv.js
@@ -1,12 +1,12 @@
-var card = $(".card");
+//var card = $(".card");
 var span = $("span");
 
 //expand cards to see content
-
+/*
 card.click(function(e){
 	$(this).toggleClass("expanded");
 });
-
+*/
 //use plus icon to expand menu when viewing on mobile
 span.click(function(e){
 	$(this.parentNode).toggleClass("menu-expanded");
