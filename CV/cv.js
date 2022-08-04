48833e21ac986b7f93a2e9f4be511f75f8be932b
Rich Wright <rchwrght@gmail.com>
2022-08-04 12:03:30 +0200

    styling navbar
    added dragon icon
    removed personal JS code that was expanding menu to use Bootstrap classes and JS

--- CV/cv.js
+++ CV/cv.js
@@ -1,14 +1,15 @@
 //var card = $(".card");
-var span = $("span");
+/*var span = $("span");
 
 //expand cards to see content
-/*
+
 card.click(function(e){
 	$(this).toggleClass("expanded");
 });
-*/
+
 //use plus icon to expand menu when viewing on mobile
 span.click(function(e){
 	$(this.parentNode).toggleClass("menu-expanded");
 	$(this).toggleClass("rotated");
 });
+*/
